c8552b349c1ed126386b2741bf83003f68476422
vivek22117 <vivekmishra22117@gmail.com>
2021-08-08 20:17:07 +0530

    updating TF providers

--- aws-infra/aws-ecs-service/providers.tf
+++ aws-infra/aws-ecs-service/providers.tf
@@ -1,31 +1,52 @@
+####################################################
+# AWS provider configuration                       #
+####################################################
 provider "aws" {
-  region = var.default_region
-//    profile = var.profile
+  region  = var.default_region
 
-  version = ">=2.35"
 }
 
-provider "template" {
-  version = "2.1.2"
-}
-
-provider "archive" {
-  version = "1.2.2"
-}
 
 ###########################################################
 # Terraform configuration block is used to define backend #
 # Interpolation syntax is not allowed in Backend          #
 ###########################################################
 terraform {
-  required_version = ">=0.12"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=3.3.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 1.3"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 2.0"
+    }
+    template = {
+      source  = "hashicorp/template"
+      version = "~> 2.0"
+    }
+    archive = {
+      source = "hashicorp/archive"
+      version = "2.2.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.0"
+    }
+  }
+  required_version = ">= 0.13"
+
 
   backend "s3" {
-    profile        = "admin"
-    bucket         = "doubledigit-tfstate-qa-us-east-1"
-    dynamodb_table = "doubledigit-tfstate-qa-us-east-1"
-    key            = "state/qa/ecs-service/config-server-app/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
+    region  = "us-east-1"
+    encrypt = "true"
   }
+
 }
+
+# used for accessing Account ID and ARN
+data "aws_caller_identity" "current" {}
